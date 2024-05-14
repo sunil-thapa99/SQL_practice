095f8ab3af9969ada09693e48c19ca832c0d5d5d
Sunil Thapa <lappanchappan43@gmail.com>
2024-05-14 16:20:45 -0400

    music store analysis pracitce

---
+++ music_store_analysis/query.sql
@@ -0,0 +1,66 @@
+/* Q1: Who is the senior most employee based on job title? */
+select * from employee
+order by levels desc
+limit 1;
+
+
+-- Q2: Which countries have the most invoices?
+
+select count(*) as c, billing_country 
+from invoice
+group by billing_country
+order by c desc
+
+-- Q3 What are top 3 values of total invoice
+
+select total from invoice
+order by total desc
+limit 3;
+
+/* Q4: Which city has the best customers? We would like to throw a promotional Music Festival in the city we made the most money. 
+Write a query that returns one city that has the highest sum of invoice totals. 
+Return both the city name & sum of all invoice totals */
+
+select sum(total) as invoice_total, billing_city
+from invoice
+group by billing_city
+order by invoice_total desc;
+
+/* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
+Write a query that returns the person who has spent the most money.*/
+
+select customer.customer_id, customer.first_name, customer.last_name, sum(invoice.total) as total
+from customer
+join invoice 
+ON customer.customer_id=invoice.customer_id
+group by customer.customer_id
+order by total desc
+limit 1;
+
+/* Q6: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+Return your list ordered alphabetically by email starting with A. */
+
+select distinct email, first_name, last_name
+from customer
+join invoice on customer.customer_id=invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id
+where track_id in (
+	select track_id
+	from track
+	join genre on track.genre_id=genre.genre_id
+	where genre.name like 'Rock'
+)
+order by email;
+
+/* Q7: Let's invite the artists who have written the most rock music in our dataset. 
+Write a query that returns the Artist name and total track count of the top 10 rock bands. */
+
+select artist.artist_id, artist.name, count(artist.artist_id) as total_songs
+from track
+join album on album.album_id = track.album_id
+join artist on artist.artist_id=album.artist_id
+join genre on genre.genre_id=track.genre_id
+where genre.name like 'Rock'
+group by artist.artist_id
+order by total_songs desc
+limit 10;
