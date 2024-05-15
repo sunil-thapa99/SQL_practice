04beed456ec823a7b1987601caacd930c73e79a7
Sunil Thapa <lappanchappan43@gmail.com>
2024-05-15 01:10:47 -0400

    Advance query 1

--- music_store_analysis/query.sql
+++ music_store_analysis/query.sql
@@ -64,3 +64,38 @@ where genre.name like 'Rock'
 group by artist.artist_id
 order by total_songs desc
 limit 10;
+
+/* Q8: Return all the track names that have a song length longer than the average song length. 
+Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first. */
+
+select name, milliseconds
+from track
+where milliseconds > (
+	select avg(milliseconds) as avg_track_length
+	from track
+)
+order by milliseconds desc;
+
+/* Q9: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
+
+WITH best_selling_artist AS (
+	select artist.artist_id as artist_id, artist.name as artist_name, 
+	sum(invoice_line.unit_price*invoice_line.quantity) as total_sales
+	from invoice_line
+	join track on track.track_id=invoice_line.track_id
+	join album on album.album_id=track.album_id
+	join artist on artist.artist_id=album.artist_id
+	group by 1
+	order by 3 desc
+	limit 1
+)
+select c.customer_id, c.first_name, c.last_name, bsa.artist_name, 
+sum(il.unit_price*il.quantity) as amount_spent
+from invoice i
+join customer c on c.customer_id=i.customer_id
+join invoice_line il on il.invoice_id=i.invoice_id
+join track t on t.track_id=il.track_id
+join album alb on alb.album_id=t.album_id
+join best_selling_artist bsa on bsa.artist_id=alb.artist_id
+group by 1, 2, 3, 4
+order by 5 desc;
