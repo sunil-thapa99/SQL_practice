ce9cf20212d7d70f238a42bb13162f766d186299
Sunil Thapa <lappanchappan43@gmail.com>
2024-05-17 20:33:10 -0400

    Aggregator completed

--- udacity_SQL/aggregations.sql
+++ udacity_SQL/aggregations.sql
@@ -189,12 +189,32 @@ We would now like to perform a similar calculation to the first, but we want to 
 spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the 
 top spending customers listed first.
 */
+SELECT a.name, SUM(total_amt_usd) total_spent, 
+        CASE WHEN SUM(total_amt_usd) > 200000 THEN 'top'
+        WHEN  SUM(total_amt_usd) > 100000 THEN 'middle'
+        ELSE 'low' END AS customer_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+WHERE occurred_at > '2015-12-31' 
+GROUP BY 1
+ORDER BY 2 DESC;
 
 /*
 We would like to identify top performing sales reps, which are sales reps associated with more than 
 200 orders. Create a table with the sales rep name, the total number of orders, and a column with top or 
 not depending on if they have more than 200 orders. Place the top sales people first in your final table.
 */
+SELECT s.name, COUNT(*) num_ords,
+        CASE WHEN COUNT(*) > 200 THEN 'top'
+        ELSE 'not' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY 2 DESC;
 
 /*
 The previous didn't account for the middle, nor the dollar amount associated with the sales. Management 
@@ -205,3 +225,14 @@ name, the total number of orders, total sales across all orders, and a column wi
 depending on this criteria. Place the top sales people based on dollar amount of sales first in your final 
 table. You might see a few upset sales people by this criteria!
 */
+SELECT s.name, COUNT(*), SUM(o.total_amt_usd) total_spent, 
+        CASE WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+        WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+        ELSE 'low' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY 3 DESC;
